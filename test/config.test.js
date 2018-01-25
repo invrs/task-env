7dd154d1dd1c75113122f8b42d6fbc5e13946e42
Winton Welsh <mail@wintoni.us>
2018-01-25 01:15:06 -0500

    More config#load tests

--- test/config.test.js
+++ test/config.test.js
@@ -1,13 +1,53 @@
 import { resolve } from "path"
 import { load } from "../lib/config"
 
-test("ok", async () => {
+test("load", async () => {
   let config = {}
   let { json, jsonMap } = await load({
     config,
     jsonDir: resolve(__dirname, "./fixture"),
   })
-  expect(config).toEqual({ default: { fizz: {} } })
-  expect(json).toEqual({ fizz: {} })
-  expect(jsonMap).toEqual({ "fizz.json": ["fizz"] })
+  expect(config).toEqual({
+    default: { bang: {}, buzz: {}, fizz: {} },
+  })
+  expect(json).toEqual({
+    ">>? condition": { condition: true },
+    bang: {},
+    buzz: {},
+    fizz: {},
+  })
+  expect(jsonMap).toEqual({
+    "bang.json": ["bang"],
+    "buzz.json": ["buzz"],
+    "fizz.json": [">>? condition", "fizz"],
+  })
+})
+
+test("load w/ conditions", async () => {
+  let config = {}
+  let { json, jsonMap } = await load({
+    config,
+    conditions: ["condition"],
+    jsonDir: resolve(__dirname, "./fixture"),
+  })
+  expect(config).toEqual({
+    condition: {
+      bang: { condition: true },
+      buzz: { condition: true },
+      condition: true,
+      fizz: { condition: true },
+    },
+    default: { bang: {}, buzz: {}, fizz: {} },
+  })
+  expect(json).toEqual({
+    ">>? condition": { condition: true },
+    bang: {},
+    buzz: {},
+    fizz: {},
+  })
+  expect(jsonMap).toEqual({
+    "bang.json": ["bang"],
+    "buzz.json": ["buzz"],
+    "fizz.json": [">>? condition", "fizz"],
+  })
 })
